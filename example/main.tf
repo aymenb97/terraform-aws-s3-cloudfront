b95952a4139ececf81e434d4f3f2439961a1deae
mcetn <ayman.bellakhder@pi-square.io>
2022-05-09 11:59:15 +0100

    added example static files

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,7 @@
 module "aws-s3-cloudfront" {
-  source = "../s3-cloudfront"
+  source        = "../s3-cloudfront"
+  path_to_files = "./static-website-files"
+}
+output "cloudfront_dns" {
+  value = module.aws-s3-cloudfront.cloudfront_domain_name
 }
